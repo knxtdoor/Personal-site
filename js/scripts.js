af0e7d6b45401b9a88467f1ba21a59ab07ca506f
Zach Slaton <knxtdoor@aol.com>
2024-06-19 02:50:17 -0400

    added scroll arrow indicator

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,19 @@ function observerStuff() {
     const slideIntoViewport = (item, observer) => {
         let listItems = [...document.getElementsByClassName("slide")];
 
-        if (item[0].boundingClientRect.top < 0) { //Above viewport
+        if (item[0].boundingClientRect.top < 0) {
+            //Above viewport
             listItems.forEach((entry) => {
                 entry.classList.toggle("skip-slide", true);
             });
-
         }
         if (item[0].isIntersecting) {
             listItems.forEach((entry) => {
                 entry.classList.toggle("slide-in", item[0].isIntersecting);
             });
+            document
+                .getElementById("scroll-arrow")
+                .classList.toggle("faded", true);
         }
     };
 
@@ -22,17 +25,15 @@ function observerStuff() {
     // });
     obs.observe(document.getElementById("experience"), {});
 
-
-
     const fadeIntoViewport = (item, observer) => {
         let skillItems = document.getElementsByClassName("skill-box");
         skillItems = [...skillItems];
         if (item[0].isIntersecting) {
             skillItems.forEach((skill) => {
                 skill.classList.toggle("faded", item[0].isIntersecting);
-            })
+            });
         }
-    }
+    };
 
     const obs2 = new IntersectionObserver(fadeIntoViewport);
     obs2.observe(document.getElementById("skills"), {});
@@ -40,32 +41,28 @@ function observerStuff() {
     let skillItems = document.getElementsByClassName("skill-box");
     skillItems = [...skillItems];
     skillItems.forEach((skill, ndx) => {
-        let delay = 1 + ndx * .5
-        skill.style.transition = delay + "s"
+        let delay = 1 + ndx * 0.5;
+        skill.style.transition = delay + "s";
 
         let bar = [...skill.getElementsByTagName("rect")][0];
         console.log(bar);
-        bar.style["transition-delay"] = (delay) + "s"
-    })
-
-
+        bar.style["transition-delay"] = delay + "s";
+    });
 }
 
-
 function skillBars() {
-
     let skills = document.getElementsByClassName("skill");
-    let baseColor = "#008080"
+    let baseColor = "#008080";
     skills = [...skills];
     skills.forEach((skill, ndx) => {
         newSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        newSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+        newSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
         newSvg.setAttribute("viewBox", "0 0 200 20");
-        newSvg.setAttribute("class", "skill-bar right")
+        newSvg.setAttribute("class", "skill-bar right");
 
-        let isRight = skill.classList.contains("right")
-        let width = (parseFloat(skill.getAttribute("level")) / 10) * 200
-        let startPos = isRight ? 200 - width : 0
+        let isRight = skill.classList.contains("right");
+        let width = (parseFloat(skill.getAttribute("level")) / 10) * 200;
+        let startPos = isRight ? 200 - width : 0;
         let color = darken(baseColor, ndx * 15);
         bar = document.createElementNS("http://www.w3.org/2000/svg", "rect");
         bar.setAttribute("x", "" + startPos);
@@ -75,33 +72,34 @@ function skillBars() {
         bar.style.height = "100%";
         bar.style.width = "" + width;
         bar.setAttribute("fill", color);
-        bar.setAttribute("class", isRight ? "right" : "")
+        bar.setAttribute("class", isRight ? "right" : "");
 
         newSvg.appendChild(bar);
         if (isRight) {
-            skill.before(newSvg)
+            skill.before(newSvg);
         } else {
             skill.after(newSvg);
         }
-
-    })
+    });
 }
 
 function darken(color, amount) {
     let { r, g, b } = hexToRgb(color);
-    let newR = r - amount > 0 ? r - amount : 0
-    let newG = g - amount > 0 ? g - amount : 0
-    let newB = b - amount > 0 ? b - amount : 0
+    let newR = r - amount > 0 ? r - amount : 0;
+    let newG = g - amount > 0 ? g - amount : 0;
+    let newB = b - amount > 0 ? b - amount : 0;
     return rgbToHex(newR, newG, newB);
 }
 
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
+    return result
+        ? {
+              r: parseInt(result[1], 16),
+              g: parseInt(result[2], 16),
+              b: parseInt(result[3], 16),
+          }
+        : null;
 }
 function componentToHex(c) {
     var hex = c.toString(16);
